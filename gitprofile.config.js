a0d8bc83b21af41589a140ef523e53e1e9c59d12
ARSAN <chris@arsancg.com>
2023-08-06 20:14:02 -0600

    change resume url

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -32,7 +32,7 @@ const config = {
 
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://drive.google.com/file/d/1l9IEuiGeIzf-Nn80EOXMKsR9jakR5fRr/view?usp=drive_link', // Empty fileUrl will hide the `Download Resume` button.
   },
 
   skills: [
