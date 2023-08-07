15362a3813726e17351b752d3240c16c37779bcb
ARSAN <chris@arsancg.com>
2023-08-06 18:32:30 -0600

    minor changes

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,7 +2,7 @@
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
+    username: 'while-basic', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
@@ -11,22 +11,22 @@ const config = {
     },
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
+    linkedin: '',
+    twitter: '',
+    mastodon: '',
     facebook: '',
     instagram: '',
     youtube: '', // example: 'pewdiepie'
     dribbble: '',
     behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
+    medium: '',
+    dev: '',
     stackoverflow: '', // example: '1/jeff-atwood'
     skype: '',
     telegram: '',
-    website: 'https://arifszn.com',
+    website: 'https://chriscelaya.xyz',
     phone: '',
-    email: 'arifulalamszn@gmail.com',
+    email: 'chris@chriscelaya.xyz',
   },
   resume: {
     fileUrl:
@@ -50,11 +50,11 @@ const config = {
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
+      company: 'Microsoft',
+      position: 'Data Center Technician II',
       from: 'September 2021',
-      to: 'Present',
-      companyLink: 'https://example.com',
+      to: '2022',
+      companyLink: 'https://microsoft.com',
     },
     {
       company: 'Company Name',
@@ -74,8 +74,8 @@ const config = {
   ], */
   education: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
+      institution: 'El Paso Community College',
+      degree: 'Associate',
       from: '2015',
       to: '2019',
     },
@@ -90,24 +90,88 @@ const config = {
   // To hide the `My Projects` section, keep it empty.
   externalProjects: [
     {
-      title: 'Project Name',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
+      title: 'BeatSaber MIDI Controller',
+      description: `MIDI-BS Controller is a midi controller for creating beat maps for the virtual reality game Beat Saber using a Novation MIDI Controller.`,
+      imageUrl: '',
+      link: 'https://youtu.be/fW6c3pb60kE',
     },
     {
-      title: 'Project Name',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
+      title: 'UniMe',
+      description: `Connecting NFT's and crypto currency through Metamask and Unity, enabling players to own, trade, and monetize unique in-game web3 wallets, characters, and assets securely.`,
+      imageUrl: '',
+      link: 'https://youtu.be/3EK6BbkEthA',
+    },
+    {
+      title: 'Blockchain Domain Name Service',
+      description: `Deployed a domain name service on the Polygon Network, enabling users to connect Metamask wallet's to their personal domains.`,
+      imageUrl: '',
+      link: 'https://www.youtube.com/watch?v=W3PUwbJamiY&t=2s',
+    },
+    {
+      title: 'Facebook Profile Scrape Tool',
+      description: `Testing vulnerabilities on Facebook.com.`,
+      imageUrl: '',
+      link: 'https://www.youtube.com/watch?v=RPDdzpXB-2A',
+    },
+    {
+      title: 'Custom Beat Saber Map',
+      description: `Deployed a domain name service on the Polygon Network, enabling users to connect Metamask wallet's to their personal domains.`,
+      imageUrl: '',
+      link: 'https://www.youtube.com/watch?v=_lwYn8YuRYs',
+    },
+    {
+      title: 'QUARANTINE 2020',
+      description: `Released an LP album during the COVID-19 lockdown in 2020. Tracks Available on Spotify, TikTok, Instagram, SoundCloud and many others.`,
+      imageUrl: '',
+      link: 'https://www.instagram.com/p/CcZm_2yjeuY/?utm_source=ig_web_copy_link',
+    },
+    {
+      title: 'Varavyshka.com',
+      description: `Created a flower eCommerce store with my colleague in Russia. Site is down due to recent events.`,
+      imageUrl: '',
+      link: 'http://varavyshka.com/',
+    },
+    {
+      title: 'Stable Diffusion Artwork',
+      description: `Quick showcase of artwork I've created using stable diffusion.`,
+      imageUrl: '',
+      link: 'https://www.instagram.com/reel/Csna5ttOA1J/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==',
+    },
+    {
+      title: 'Stable Diffusion Animation',
+      description: `Short video animation.`,
+      imageUrl: '',
+      link: 'https://www.instagram.com/p/CmNF_iYjR3L/',
+    },
+    {
+      title: 'Unreal Engine 5 Enviornment',
+      description: `Developed small forest environment, created camera pan movements and angles.`,
+      imageUrl: '',
+      link: 'https://www.instagram.com/p/Ch6W2jMjjnc/',
+    },
+    {
+      title: 'Blender Video Animation',
+      description: `Created a short static shot of a metallic ball in levitation.`,
+      imageUrl: '',
+      link: 'https://www.instagram.com/p/ChrG4v_jgls/',
+    },
+    {
+      title: '2009 Nissan 350z Engine Build',
+      description: `Built a V6 naturally aspirated VQ35DE engine for my Nissan 350z.`,
+      imageUrl: '',
+      link: 'https://www.instagram.com/p/Bzy2RRqhB6I/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==',
+    },
+    {
+      title: 'Wi-Fi Deauthenticator',
+      description: `Developed a wireless toolkit to create nonsensical hotspot names, deauthenticate, and more.`,
+      imageUrl: '',
+      link: 'https://www.instagram.com/p/CE62Nq8hjD_/?img_index=1',
     },
   ],
   // Display blog posts from your medium or dev account. (Optional)
   blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
+    source: '', // medium | dev
+    username: '', // to hide blog section, keep it empty
     limit: 2, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
@@ -119,7 +183,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'wireframe',
+    defaultTheme: 'winter',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
@@ -177,13 +241,6 @@ const config = {
       '--rounded-btn': '3rem',
     },
   },
-
-  // Optional Footer. Supports plain text or HTML.
-  footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
-      target="_blank"
-      rel="noreferrer"
-    >GitProfile</a> and ❤️`,
 };
 
 export default config;
